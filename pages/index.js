da389c6bcaf5b9022a5d813f5cb93ab860781084
manhcuongdtbk <manhcuongdtbk@gmail.com>
2021-08-29 00:06:57 +0700

    Adding Head contents for non dynamic pages

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import {getFeaturedEvents} from "../helpers/api-utils"
 import EventList from "../components/events/event-list"
+import Head from "next/head"
 
 function HomePage(props) {
   return (
     <div>
+      <Head>
+        <meta name='description' content='Find a lot of great events that allow you to evolve'/>
+      </Head>
       <EventList items={props.events}/>
     </div>
   )
